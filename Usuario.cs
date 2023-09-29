867e45735ab811767881774bdbd79082e4934bfa
RdzJJ <july25rodriguezjimenez2509@gmail.com>
2023-09-28 22:23:17 -0500

    Cambios en Usuario

--- Usuario.cs
+++ Usuario.cs
@@ -7,28 +7,29 @@
         public string NroCuenta = "";
         private string Contrasena = "";
         private string Celular = "";
-        private double saldo = 0;
+        private double Saldo = 0;
         public Cuenta cuenta;
 
         public Usuario()
         {
             Nombre = "";
             CC = "";
-            saldo = 0;
+            Saldo = 0;
             Contrasena = "";
             NroCuenta = "";
             Celular = "";
         }
 
-        public Usuario(string nombre, string cédulaCiudadania, string Contrasena, string celular)
+        public Usuario(string nombre, string cc, string Contrasena, string celular, double saldo)
         {
             Random random = new Random();
             this.Nombre = nombre;
-            this.CC = cédulaCiudadania;
+            this.CC = cc;
             this.Contrasena = Contrasena;
+            this.Saldo = saldo;
             this.NroCuenta = random.Next(0000000000, 999999999).ToString();
             this.Celular = celular;
-            this.cuenta = new Cuenta(this.NroCuenta,this.CC,this.Nombre,this.saldo);
+            this.cuenta = new Cuenta(NroCuenta, cc, nombre, saldo);
         }
 
         public string getCC()
@@ -50,21 +51,59 @@
         {
             this.Celular = celular;
         }
-        public bool ActualizarDatos(string celular, string contrasena)
+
+        public string getContrasena()
         {
-            if (!this.Celular.Equals(celular))
-            {
-                this.Celular = celular;
-                return true;
-            }
-            else if (!this.Contrasena.Equals(contrasena))
-            {
-                this.Contrasena = contrasena;
-                return true;
-            }
-            return false;
+            return Contrasena;
         }
-        public void CrearCuenta() { }
-        public void BorrarCuenta() { }  
+
+        public void setContrasena(string contrasena)
+        {
+            Contrasena = contrasena;
+        }
+
+        public static Cuenta GetCuenta(string cuenta)
+        {
+            List<Usuario> lista = Mostrar();
+            Usuario obj;
+            foreach (Usuario usuario in lista)
+            {
+                if (usuario.CC.Equals(cuenta))
+                {
+                    return usuario.cuenta;
+                }
+            }
+            return null;
+        }
+
+        public static int VerificarUsuario(string usuario)
+        {
+            List<Usuario> lista = Mostrar();
+            for(int i = 0; i < lista.Count; i++)
+            {
+                if (lista[i].CC.Equals(usuario))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public static int VerificarInicio(string usuario, string contrasena)
+        {
+            List<Usuario> lista = Mostrar();
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (lista[i].CC.Equals(usuario) && lista[i].CC.Equals(contrasena))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public static List<Usuario> Mostrar()
+        {
+            return UserList.Mostrar();
+        }
     }
 }
